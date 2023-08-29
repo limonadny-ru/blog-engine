8df0aa42405dcc9082e213dcddfbf1666981b026
Maxim <choochooh55@gmail.com>
2023-08-29 22:11:19 +0300

    1:n src/blog_engine/core.clj 1:n text/туду.txt

--- src/blog_engine/core.clj
+++ src/blog_engine/core.clj
@@ -99,13 +99,6 @@
             (re-find img-regexp (first s))))))))
 
 
-(comment
-  
-  (compile-images (slurp "text/туду.txt"))
-  
-  )
-
-
 ;;
 ;; Compilators
 ;;
@@ -163,7 +156,23 @@
        " —"
        "&nbsp;—")
      
-     
+     content
+     (str/replace 
+       content
+       #"(?i)\b((?:https?:(?:/{1,3}|[a-zа-я0-9%])|[a-zа-я0-9.\-]+[.](?:рф|com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’])|(?:(?<!@)[a-zа-я0-9]+(?:[.\-][a-zа-я0-9]+)*[.](?:рф|com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"
+       (fn [s]
+         (if 
+           (re-find 
+             #"http://|https://"
+             (first s))
+           (format 
+             "<a href='%s'>%s</a>"
+             (first s)  
+             (first s))
+           (format 
+             "<a href='http://%s'>%s</a>"
+             (first s)  
+             (first s)))))
      
      content
      (str 
@@ -179,6 +188,25 @@
        (slurp footer))]
     
     (spit (str out "/" title ".html") content)))
+
+
+(comment
+  (re-seq
+    
+    (slurp "text/туду.txt"))
+  
+  (re-seq
+     #"[-a-zа-яА-ЯA-Z0-9@:%._\+~#=]{1,256}\.[a-zа-яА-ЯA-Z0-9()]{1,6}\b(?:[-a-zа-яА-ЯA-Z0-9()@:%_\+.~#?&//=]*)"
+    (slurp "text/туду.txt"))
+  
+  
+  (str/replace 
+       (slurp "text/туду.txt")
+       #"^https?:\/\/(?:www\.)?[-a-zа-яА-ЯA-Z0-9@:%._\+~#=]{1,256}\.[a-zа-яА-ЯA-Z0-9()]{1,6}\b(?:[-a-zа-яА-ЯA-Z0-9()@:%_\+.~#?&\/=]*)$"
+       (fn [s]
+         (format 
+           "<a href='%s'>%s</a>"
+           s s))))
 
 
 (defn compile-posts
